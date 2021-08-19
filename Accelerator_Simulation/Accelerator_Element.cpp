78095705061322e74f5aac076e0c25d98f428517
GuoCheng <15108484007@qq.com>
2021-08-19 15:53:44 +0800

    增加矩形磁铁类

--- Accelerator_Simulation/Accelerator_Element.cpp
+++ Accelerator_Simulation/Accelerator_Element.cpp
@@ -1,11 +1,6 @@
 #include "Accelerator_Element.h"
 #include "PhysicalParameter.h"
 
-Drift::Drift(double length)
-{
-	this->length = length;
-}
-
 void Drift::CalculateTransferMatrix()
 {
 	double gamma = this->p_beam->p_IdealParticle->GetGamma();
@@ -38,18 +33,13 @@ void Accelerator_Element::SetBeam(Beam& beam)
 	this->p_beam = &beam;
 }
 
-Matrix Accelerator_Element::GetTransferMatrix()
+Matrix& Accelerator_Element::GetTransferMatrix()
 {
 	CalculateTransferMatrix();
 	return TransferMatrix;
 }
 
-SBend::SBend(double length)
-{
-	this->length = length;
-}
-
-void SBend::SetTheta(double Theta)
+void Bend::SetTheta(double Theta)
 {
 	this->Theta = Theta;
 	this->rho = this->length / Theta;
@@ -93,26 +83,21 @@ void SBend::CalculateTransferMatrix()
 			-sin(Theta)/rho,	cos(Theta),					0,				0,				0,		sin(Theta),
 			0,					0,							1,				length,			0,		0,
 			0,					0,							0,				1,				0,		0,
-			-sin(Theta),		-(1 - cos(Theta)) * rho,	0,				0,				1,		-(beta * beta * length - sin(Theta) * rho),
+			-sin(Theta),		-(1 - cos(Theta)) * rho,	0,				0,				1,		sin(Theta)* rho - beta * beta * length,
 			0,					0,							0,				0,				0,		1
 		};
 		TransferMatrix.InputMatrixBuffer(buffer, sizeof(buffer));
 	}
 }
 
-void SBend::CalculateThroughB()
+void Bend::CalculateThroughB()
 {
 	SetTheta(- B * length / p_beam->p_IdealParticle->GetB_rho());
 }
 
-void SBend::CalculateThroughg()
+void Bend::CalculateThroughg()
 {
 	this->n = - rho / B * g;
-}
-
-Quad::Quad(double length)
-{
-	this->length = length;
 }
 
 void Quad::Setk_Mquad(double k_Mquad)
@@ -158,4 +143,48 @@ void Quad::CalculateThroughB()
 void Quad::CalculateThroughg()
 {
 	k_Mquad = g / p_beam->p_IdealParticle->GetB_rho();
+}
+
+void RBend::CalculateTransferMatrix()
+{
+	double Kx = sqrt((1 - n) / rho / rho), Ky = sqrt(n / rho / rho);
+	double temp1 = Kx * length, temp2 = Ky * length;
+	double gamma = p_beam->p_IdealParticle->GetGamma();
+	double beta = p_beam->p_IdealParticle->GetBeta();
+	Matrix temp_SBend(6, 6);
+	if (Ky != 0) {
+		double buffer[36] = {
+			cos(temp1),				sin(temp1) / Kx,					0,					0,				0,		(1 - cos(temp1)) / rho / Kx / Kx,
+			-Kx * sin(temp1),		cos(temp1),							0,					0,				0,		sin(temp1) / rho / Kx,
+			0,						0,									cos(temp2),			sin(temp2) / Ky,0,		0,
+			0,						0,									-Ky * sin(temp2),	cos(temp2),		0,		0,
+			-sin(temp1) / rho / Kx,	-(1 - cos(temp1)) / rho / Kx / Kx,	0,					0,				1,		-((1 - rho * rho * Kx * Kx / gamma / gamma) * length - sin(temp1) / Kx) / rho / rho / Kx / Kx,
+			0,						0,									0,					0,				0,		1
+		};
+		temp_SBend.InputMatrixBuffer(buffer, sizeof(buffer));
+	}
+	else
+	{
+		double buffer[36] = {
+			cos(Theta),			sin(Theta) * rho,			0,				0,				0,		(1 - cos(Theta)) * rho,
+			-sin(Theta) / rho,	cos(Theta),					0,				0,				0,		sin(Theta),
+			0,					0,							1,				length,			0,		0,
+			0,					0,							0,				1,				0,		0,
+			-sin(Theta),		-(1 - cos(Theta)) * rho,	0,				0,				1,		sin(Theta) * rho - beta * beta * length,
+			0,					0,							0,				0,				0,		1
+		};
+		temp_SBend.InputMatrixBuffer(buffer, sizeof(buffer));
+	}
+	
+	Matrix temp_Deflection(6, 6);
+	double buffer2[36] = {
+		1.0,						0.0,	0.0,							0.0,	0.0,	0.0,
+		tan(0.5 * Theta) / rho,		1.0,	0.0,							0.0,	0.0,	0.0,
+		0.0,						0.0,	1.0,							0.0,	0.0,	0.0,
+		0.0,						0.0,	-tan(0.5 * Theta) / rho,		1.0,	0.0,	0.0,
+		0.0,						0.0,	0.0,							0.0,	1.0,	0.0,
+		0.0,						0.0,	0.0,							0.0,	0.0,	1.0
+	};
+	temp_Deflection.InputMatrixBuffer(buffer2, sizeof(buffer2));
+	TransferMatrix = temp_Deflection * temp_SBend * temp_Deflection;
 }
--- Accelerator_Simulation/Accelerator_Element.h
+++ Accelerator_Simulation/Accelerator_Element.h
@@ -10,7 +10,7 @@ public:
 	void Set_g(double g);
 	void SetBeam(Beam& beam);
 
-	Matrix GetTransferMatrix();
+	Matrix& GetTransferMatrix();
 protected:
 	Matrix TransferMatrix = Matrix(6, 6);
 	virtual void CalculateThroughB() = 0;
@@ -27,31 +27,42 @@ private:
 
 class Drift :public Accelerator_Element {
 public:
-	Drift(double length = 1.0);
+
 private:
 	virtual void CalculateTransferMatrix();
 	virtual void CalculateThroughB();
 	virtual void CalculateThroughg();
 };
 
-class SBend :public Accelerator_Element {
+class Bend:public Accelerator_Element
+{
 public:
-	SBend(double length = 1.0);
-
 	void SetTheta(double Theta);
+
+protected:
+	double Theta;	//rad
+	double rho;		//m
+	double n;		// rho/B*B'
+
 private:
-	virtual void CalculateTransferMatrix();
 	virtual void CalculateThroughB();
 	virtual void CalculateThroughg();
+};
 
-	double Theta = 0;//rad
-	double rho = 1;//m
-	double n;// rho/B*B'
+class SBend :public Bend {
+public:
+
+private:
+	virtual void CalculateTransferMatrix();
+};
+class RBend :public Bend {
+public:
+private:
+	virtual void CalculateTransferMatrix();
 };
 
 class Quad :public Accelerator_Element {
 public:
-	Quad(double length = 1.0);
 	void Setk_Mquad(double k_Mquad);
 
 private:
